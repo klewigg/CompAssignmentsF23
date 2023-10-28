3831c268ae7000d19d0cacc33416caeab164bda0
Kai Le <kaile@Kais-MacBook-Pro.local>
2023-10-28 14:49:40 -0400

    Le-Comptypescript

--- typescript/test.js
+++ typescript/test.js
@@ -2,5 +2,14 @@ function square(number) {
     return number * number;
 }
 function numberString(i) {
+    if (i < 0) {
+        return -1;
+    }
+    else {
+        while (i > 0) {
+            console.log(i - 1);
+            i--;
+        }
+    }
     return "";
 }
--- typescript/test.ts
+++ typescript/test.ts
@@ -1,8 +1,21 @@
-function square(n) {
-    return 0;
+function square(number: number): number 
+{
+    return number * number;
 }
 
 
-function numberString(i) {
-    return ""; 
+function numberString(i: number): string | number {
+    if (i < 0) 
+    {
+        return -1;
+    } 
+    else 
+    {
+        while (i > 0) 
+        {
+            console.log(i - 1);
+             i--;
+        }
+    }
+    return "";
 }
